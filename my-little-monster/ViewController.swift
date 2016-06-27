c579474a8d57644c0f990834bd1adfb47925c825
Adam Goth <adamgoth@gmail.com>
2016-06-27 07:05:41 -0600

    Adds animation to monster

--- my-little-monster/ViewController.swift
+++ my-little-monster/ViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImg: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var imgArray = [UIImage]()
+        for x in 1...4 {
+            let img = UIImage(named: "idle\(x).png")
+            imgArray.append(img!)
+        }
+        
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
         
     }
     
